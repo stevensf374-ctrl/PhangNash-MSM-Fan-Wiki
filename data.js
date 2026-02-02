3b9073b38590273a84671044898f48ed4d788829
stevensf374-ctrl <stevensf374@gmail.com>
2026-02-02 01:14:23 -0600

    Update data.js

--- data.js
+++ data.js
@@ -2,11 +2,17 @@ const monsters = [
   {
     name: 'Furcorn',
     type: 'monster',
+    rarity: 'common',
     elements: ['Plant','Cold'],
     sound: 'Soft vocal harmony',
     image: 'images/furcorn.png',
     summary: 'A friendly Natural Monster known for gentle singing.',
     cost: '30 diamonds','300 diamonds (Shugabush Island)','35 diamonds (Colossingum)','4,000 coins (Composer Island)'
+    baseBreedTime: 8, // hours
+    islands: {
+      'Plant Island': { rule: 'Standard breeding' },
+      'Shugabush Island': { rule: 'Shugabush + any monster' }
+    },
     breeding: ['Potbelly + Mammott' , 'Furcorn + Rare Furcorn']
   }
 ];
