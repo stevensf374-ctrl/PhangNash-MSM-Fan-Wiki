1b54eee1bc14e6d208066b035c4da6263f1dd089
stevensf374-ctrl <stevensf374@gmail.com>
2026-02-02 00:26:26 -0600

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,9 @@ const monsters = [
     elements: ['Plant','Cold'],
     sound: 'Soft vocal harmony',
     image: 'images/furcorn.png',
-    summary: 'A friendly Natural Monster known for gentle singing.'
+    summary: 'A friendly Natural Monster known for gentle singing.',
+    cost: '30 diamonds','300 diamonds (Shugabush Island)','35 diamonds (Colossingum)','4,000 coins (Composer Island)'
+    breeding: ['Potbelly + Mammott' , 'Furcorn + Rare Furcorn']
   }
 ];
 
